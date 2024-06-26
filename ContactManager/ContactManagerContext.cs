8231b4f6ab47f99484abf78930719ee421450479
Vishal <vishal.pawar@moreyeahs.com>
2024-06-26 14:21:41 +0530

    Initial Setup with Entity Framework and add gitingore file

---
+++ ContactManager/ContactManagerContext.cs
@@ -0,0 +1,21 @@
+﻿using ContactManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManager
+{
+    public class ContactManagerContext : DbContext
+    {
+        public ContactManagerContext(DbContextOptions<ContactManagerContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ContactManagerContext).Assembly);
+        }
+
+        public virtual DbSet<Contact> Customers { get; set; }
+
+    }
+}
---
+++ ContactManager/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContactManager.Models
+{
+    public class Contact
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+    }
+}
